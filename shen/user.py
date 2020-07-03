95d8bea9a9f323abe5975efd4fd2ff572e08eeb5
Branden Akana <branden.akana@gmail.com>
2020-07-02 16:23:41 -1000

    add nickname support

--- shen/user.py
+++ shen/user.py
@@ -1,3 +1,30 @@
+"""
+User
+====
+
+A user contains a UUID as an identifier as well as a
+username and discriminator (a 4-digit number).
+
+The form "<username>#<discriminator>" acts as a human-readable alias
+for the UUID.
+
+### Limitations
+
+- The discriminator can only be a 4-digit number (ranging from 0000 to 9999).
+- The username is case-insensitive and can only contain letters, underscore, and dash.
+
+### Nickname
+
+The _nickname_ is an optional name for this user to display
+instead of the username. There are no limitations to the nickname.
+
+### Connections
+
+A user may also contain additional data, like their Discord ID.
+This will allow a Discord user with the ID to act on
+behalf of this user.
+"""
+
 import random
 from uuid import UUID, uuid4
 
@@ -17,30 +44,34 @@ def gen_uuid() -> UUID:
 
 
 class User:
-    def __init__(self, name: str, discriminator: str = None, id: UUID = None):
+    def __init__(self,
+                 username: str,
+                 discriminator: str = None,
+                 uuid: UUID = None,
+                 nickname: str = None):
+
+        # the name of the user
+        self.username: str = username
 
         # the UUID of the user
-        if id is None:
-            id = uuid4()
-
-        self.id: UUID = id
-        # the name of the user
-        self.name: str = name
+        self.uuid = uuid or uuid4()
 
         # the discriminator (4-digit identifier) of the user
-        if discriminator is None:
-            discriminator = gen_discriminator()
+        self.discriminator: str = discriminator or gen_discriminator()
 
-        self.discriminator: str = discriminator
+        self.nickname: str = nickname or username
+
+    def get_tag(self) -> str:
+        return self.username + '#' + self.discriminator
 
     def __str__(self) -> str:
-        return self.name + '#' + self.discriminator
+        return f"({self.uuid}) {self.get_tag()}"
 
     def __eq__(self, other) -> bool:
-        if self.id == other.id:
+        if self.uuid == other.uuid:
             return True
         else:
             return self is other
 
     def __hash__(self):
-        return hash(str(self.id))
+        return hash(str(self.uuid))
